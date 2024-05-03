f9732f3d789202e1754e8da7753207f1f5fc74d4
Marcos Guibson <112142514+mguibtech@users.noreply.github.com>
2024-05-02 22:48:38 -0400

    feat: Adding custom fonts

--- App.tsx
+++ App.tsx
@@ -5,7 +5,11 @@ import { Text } from "./src/components/Text/Text";
 function App(){
   return (
     <SafeAreaView>
+      <Text bold> Testando primeiro componente</Text>
+      <Text italic bold semiBold> Testando primeiro componente</Text>
+
       <Text> Testando primeiro componente</Text>
+
     </SafeAreaView>
   );
 }
--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -7,18 +7,55 @@ import {
 
 interface TextProps extends RNTextProps {
   preset?: TextVariants;
+  bold?: boolean;
+  italic?: boolean;
+  semiBold?: boolean;
 }
+
 export function Text({
   children,
   preset = 'paragraphMedium',
+  bold,
+  italic,
+  semiBold,
   style,
   ...rest
 }: TextProps) {
+  const fontFamily = getFontFamily(preset, bold, italic, semiBold);
   return (
-    <RNText style={[$fontSizes[preset], style]} {...rest}>
+    <RNText style={[$fontSizes[preset], { fontFamily }, style]} {...rest}>
       {children}
     </RNText>
   );
+}
+
+function getFontFamily(
+  preset: TextVariants,
+  bold?: boolean,
+  italic?: boolean,
+  semiBold?: boolean,
+) {
+  if (
+    preset === 'headingLarge' ||
+    preset === 'headingMedium' ||
+    preset === 'headingSmall'
+  ) {
+    return italic ? $fontFamily.boldItalic : $fontFamily.bold;
+  }
+  switch (true) {
+    case bold && italic:
+      return $fontFamily.boldItalic;
+    case bold:
+      return $fontFamily.bold;
+    case italic:
+      return $fontFamily.italic;
+    case semiBold && italic:
+      return $fontFamily.mediumItalic;
+    case semiBold:
+      return $fontFamily.medium;
+    default:
+      return $fontFamily.regular;
+  }
 }
 
 type TextVariants =
@@ -32,14 +69,27 @@ type TextVariants =
   | 'paragraphCaptionSmall';
 
 const $fontSizes: Record<TextVariants, TextStyle> = {
-  headingLarge: {fontSize: 32, lineHeight: 38.4},
-  headingMedium: {fontSize: 22, lineHeight: 26.4},
-  headingSmall: {fontSize: 18, lineHeight: 23.4},
+  headingLarge: { fontSize: 32, lineHeight: 38.4 },
+  headingMedium: { fontSize: 22, lineHeight: 26.4 },
+  headingSmall: { fontSize: 18, lineHeight: 23.4 },
 
-  paragraphLarge: {fontSize: 18, lineHeight: 25.2},
-  paragraphMedium: {fontSize: 16, lineHeight: 22.4},
-  paragraphSmall: {fontSize: 14, lineHeight: 19.6},
+  paragraphLarge: { fontSize: 18, lineHeight: 25.2 },
+  paragraphMedium: { fontSize: 16, lineHeight: 22.4 },
+  paragraphSmall: { fontSize: 14, lineHeight: 19.6 },
 
-  paragraphCaption: {fontSize: 12, lineHeight: 16.8},
-  paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+  paragraphCaption: { fontSize: 12, lineHeight: 16.8 },
+  paragraphCaptionSmall: { fontSize: 10, lineHeight: 14 },
+};
+
+const $fontFamily = {
+  black: 'Sora-Black',
+  blackItalic: 'Sora-BlackItalic',
+  bold: 'Sora-Bold',
+  boldItalic: 'Sora-BoldItalic',
+  italic: 'Sora-Italic',
+  light: 'Sora-Light',
+  lightItalic: 'Sora-LightItalic',
+  medium: 'Sora-Medium',
+  mediumItalic: 'Sora-MediumItalic',
+  regular: 'Sora-Regular',
 };
