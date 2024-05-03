e4a14da704d6524ea51d1b2f7523f096c8ec68d5
Marcos Guibson <112142514+mguibtech@users.noreply.github.com>
2024-05-02 22:10:04 -0400

    feat: Initial project and create text component

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView } from "react-native";
+import { Text } from "./src/components/Text/Text";
+
+
+function App(){
+  return (
+    <SafeAreaView>
+      <Text> Testando primeiro componente</Text>
+    </SafeAreaView>
+  );
+}
+export default App;
